f36ee6762a7e5f0309729fd8a754a2c8d03df3db
AJPB4133 <andrea.jeanpaul99@gmail.com>
2022-09-30 14:29:03 +0200

    Abgabe Uebung02

---
+++ Uebung02/Uebung02.js
@@ -0,0 +1,18 @@
+//Aufgabe 01
+
+var L = []
+
+for(var i =1; i < 100; i= i+2){
+    L.push(i)
+    console.log(L);
+}
+
+//Aufgabe 02
+
+var zahl = Math.floor(5*Math.random()) + 1;
+
+function wuerfel(){
+    return (zahl) 
+}
+
+console.log(wuerfel(zahl));
