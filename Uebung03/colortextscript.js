37cd908441092b65ee5512f27c6ea52454678594
AJPB4133 <andrea.jeanpaul99@gmail.com>
2022-10-11 11:04:07 +0200

    Verbesserung colortext.html

--- Uebung03/colortextscript.js
+++ Uebung03/colortextscript.js
@@ -1,12 +1,17 @@
 "use strict";
 
 function changecolor(){
-    var txt= document.querySelector("#src");
+    var txt= document.querySelector("#src").value;
+    var txtoutput= document.querySelector("#dsc");
+
+    txtoutput.innerHTML = txt;
+
     var random = Math.floor(Math.random()*16777125).toString(16);
     var farbe = "#" + random;
-    txt.style["color"] = farbe;
-    txt.style["font-size"]= "50px";
-    txt.style["text-align"]= "center";
+
+    txtoutput.style["color"] = farbe;
+    txtoutput.style["font-size"]= "50px";
+    txtoutput.style["text-align"]= "center";
 
 
 }
